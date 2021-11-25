c688d35254ed556cb80e0c73cb486f43dba5f99d
jinaapark <59815544+jinaapark@users.noreply.github.com>
2021-11-24 22:27:12 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Todo from './Todo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Jina's ToDo App</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <><section id="myTodos">
+        <div class="content">
+        <div class="addBar">
+            <div class="toDo" id="toDoName">My ToDo List</div>
+            <div class="row" id="toDoNew">
+                <button class="col" id="toDoButton">add</button>
+            </div>
+        </div>
+        <div class="toDo" id="toDoDiv">
+            <div class="toDoList" id="toDoList"></div>
+        </div>
     </div>
+    </section>
+    <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <h1>Jina's ToDo App</h1>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div></>
   );
 }
 
